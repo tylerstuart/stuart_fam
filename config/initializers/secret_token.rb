03a3aa29b4f55ea5c377c4fd5a98c0f3a152cc45
Tyler Stuart <tstuart@1on1.com>
2013-02-21 11:24:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StuartFam::Application.config.secret_token = '4dd8056bbd99e303f7e840f5734e32ba6cbc7499b8a3e6a325ed2583626db3983067083a8a83460b90e658f46f12e3656b8aa86a4d9d4ef10bbf2bd00f790569'
